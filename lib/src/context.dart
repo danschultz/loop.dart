2e15b8892f7a4f5caeada413c83ac11c54511df3
Dan Schultz <schultz.t.dan@gmail.com>
2015-10-12 02:04:28 -0700

    Add API and example for update functions returning effects

--- lib/src/context.dart
+++ lib/src/context.dart
@@ -2,29 +2,22 @@ part of dedux;
 
 class Context<S> {
   final _actions = new Channel<Action>.broadcast();
-  final _effects = new Channel<Effect>();
+
+  EventStream<Update<S>> _updates;
 
   Property<S> _state;
   Property<S> get state => _state;
 
   Context(S initialState, Reducer<S> reducer) {
-    _state = _actions.stream.scan(initialState, (S state, action) => reducer(state, action)).asProperty();
-    _effects.stream.listen((effect) async => effect(_actions, await state.first));
+    _updates = _actions.stream
+        .scan(new Update(initialState, noEffect), (Update<S> update, action) => reducer(update.state, action));
+    _state = _updates
+        .doAction((update) => update.effect(_actions, update.state))
+        .map((update) => update.state)
+        .asProperty();
   }
 
-  void dispatch(actionOrEffect) {
-    if (actionOrEffect is Action) {
-      dispatchAction(actionOrEffect);
-    } else if (actionOrEffect is Effect) {
-      dispatchEffect(actionOrEffect);
-    }
-  }
-
-  void dispatchAction(Action action) {
+  void dispatch(Action action) {
     _actions.add(action);
-  }
-
-  void dispatchEffect(Effect effect) {
-    _effects.add(effect);
   }
 }
--- lib/src/effect.dart
+++ lib/src/effect.dart
@@ -1,3 +1,10 @@
 part of dedux;
 
 typedef Future Effect<S>(Channel<Action> actions, S state);
+
+final noEffect = (actions, state) => new Future.value();
+
+/// An abstract effect that implements the [Effect] typedef.
+abstract class BaseEffect<S> {
+  Future call(Channel<Action> actions, S state);
+}
